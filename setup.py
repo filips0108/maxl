4f77827e994ae69fd42c3467005c860fe1b6b134
Filip <filips010811@gmail.com>
2026-07-14 21:48:27 +0200

    Make 'modules' a proper package and include JSON data

--- setup.py
+++ setup.py
@@ -5,25 +5,27 @@ import pathlib
 
 HERE = pathlib.Path(__file__).parent
 
-LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding="utf-8") if (HERE / "README.md").exists() else ""
+# Read the README for the long description (optional but nice)
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding="utf-8") \
+    if (HERE / "README.md").exists() else ""
 
 setup(
     name="maxl",
     version="0.0.1",
-    description="MaxL - An interactive shell/interpreter with simulated filesystem",
+    description="MaxL – An interactive shell/interpreter with simulated filesystem",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    author="Filip",  # TODO: Update
-    url="https://github.com/filips0108/maxl",  # TODO: Update
-    packages=find_packages(),
-    include_package_data=True,
+    author="Filip Stojanović",               
+    url="https://github.com/filips0108/maxl",  
+    packages=find_packages(),         
+    include_package_data=True,        
     install_requires=[
         "prompt_toolkit>=3.0",
         "rich>=10.0",
     ],
     entry_points={
         "console_scripts": [
-            "maxl=modules.std:load",
+            "maxl=modules.std:load", 
         ],
     },
     python_requires=">=3.7",
